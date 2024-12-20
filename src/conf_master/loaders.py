be5cce7a606c9c9bb25b73b2eb43826e71a755c5
Alvaro Rodriguez <Alvaro.Rodriguez@sebn.com>
2024-12-20 11:24:46 +0100

    Lint with ruff

--- src/conf_master/loaders.py
+++ src/conf_master/loaders.py
@@ -9,16 +9,20 @@ def load_json(file_path):
     with open(file_path, "r") as f:
         return json.load(f)
 
+
 def load_yaml(file_path):
     with open(file_path, "r") as f:
         return yaml.safe_load(f)
 
+
 def load_env():
     return {key: value for key, value in os.environ.items()}
 
+
 def load_toml(file_path):
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         return toml.load(f)
+
 
 def load_ini(file_path):
     config = configparser.ConfigParser()
